5f0944d51b21fcd3bc1c5891099e102a44dcd2c8
Raymondchen0223 <raymond_chen0223@163.com>
2020-03-26 22:57:38 +0800

    配置文件

--- src/main/java/com/gzsg/microStore/MicroStoreApplication.java
+++ src/main/java/com/gzsg/microStore/MicroStoreApplication.java
@@ -1,4 +1,12 @@
 package com.gzsg.microStore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MicroStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroStoreApplication.class);
+    }
 }
